b5d1ca098e6e68cbca224f264a1e769b184f5e4c
Daniel <Daniel.Stoughton@gmail.com>
2020-05-05 21:52:00 -0500

    finalized the UI parts of the basic sample.

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,13 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Net;
+using System.Net.Http;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
-
+    public GameObject lblMyIpAddress;
+    public GameObject txtUsername;
+    public GameObject txtIpAddress;
 
     void Start()
     {
-          
+        var httpClient = new HttpClient();
+        var task = httpClient.GetStringAsync("https://api.ipify.org");
+
+        task.Wait();
+
+        lblMyIpAddress.GetComponent<Text>().text = task.Result;
+    }
+
+    void OnHostClick()
+    {
+        NetworkConfig.UserName = txtUsername.GetComponent<Text>().text;
+        NetworkConfig.IpAddress = lblMyIpAddress.GetComponent<Text>().text;
+        NetworkConfig.IsServer = true;
+
+        //TODO: move to the chat scene.
+    }
+
+    void OnJoinClick()
+    {
+        NetworkConfig.UserName = txtUsername.GetComponent<Text>().text;
+        NetworkConfig.IpAddress = lblMyIpAddress.GetComponent<Text>().text;
+        NetworkConfig.IsServer = false;
+
+        //TODO: move to the chat scene.
     }
 }
---
+++ Assets/Scripts/Networking/NetworkConfig.cs
@@ -0,0 +1,6 @@
+﻿public static class NetworkConfig
+{
+    public static string IpAddress { get; set; }
+    public static string UserName { get; set; }
+    public static bool IsServer { get; set; }
+}
